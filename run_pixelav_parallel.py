2dbf491aaf99ff02f9d1a740ecb454e3eb262005
Sarah Waldych <101739736+swaldy@users.noreply.github.com>
2026-09-22 23:07:10 -0500

    Update DATA_DIR path for pixelav files

--- run_pixelav_parallel.py
+++ run_pixelav_parallel.py
@@ -8,7 +8,8 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DATA_DIR = "./pixelav_48x192_10ps/gzip_files_48_192_center"
+#DATA_DIR = "./pixelav_48x192_10ps/gzip_files_48_192_center"
+DATA_DIR = "/eos/user/s/swaldych/smart_pix/pixelav_48x192_10ps/gzip_files_48_192_center"
 
 BINARY = os.path.join(
     BASE_DIR,
